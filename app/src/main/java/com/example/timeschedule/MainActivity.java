64861194707dc7798d94714ff8558a680f9586d7
maru13 <48304611+maru13@users.noreply.github.com>
2019-12-17 23:18:09 +0900

    通知曜日で値取得

--- app/src/main/java/com/example/timeschedule/MainActivity.java
+++ app/src/main/java/com/example/timeschedule/MainActivity.java
@@ -20,6 +20,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
 
+import java.util.Calendar;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,6 +32,33 @@ public class MainActivity extends AppCompatActivity {
     private TextView mon4;
     private TextView mon5;
 
+    private TextView tues;
+    private TextView tues1;
+    private TextView tues2;
+    private TextView tues3;
+    private TextView tues4;
+    private TextView tues5;
+
+    private TextView wend;
+    private TextView wend1;
+    private TextView wend2;
+    private TextView wend3;
+    private TextView wend4;
+    private TextView wend5;
+
+    private TextView thurs;
+    private TextView thurs1;
+    private TextView thurs2;
+    private TextView thurs3;
+    private TextView thurs4;
+    private TextView thurs5;
+
+    private TextView fri;
+    private TextView fri1;
+    private TextView fri2;
+    private TextView fri3;
+    private TextView fri4;
+    private TextView fri5;
 
 
 
@@ -176,10 +205,11 @@ public class MainActivity extends AppCompatActivity {
                         MainActivity.this,
                         "MyChannel_Id");
 
+
                 //その日のデータを取得
                 //月曜日のデータ取得
                 mon = findViewById(R.id.textView40);
-                String text = mon.getText().toString();
+                String Mon = mon.getText().toString();
                 mon1 = findViewById(R.id.text1);
                 String text1 =  mon1.getText().toString();
                 mon2 = findViewById(R.id.text6);
@@ -190,16 +220,104 @@ public class MainActivity extends AppCompatActivity {
                 String text4 =  mon4.getText().toString();
                 mon5 = findViewById(R.id.text21);
                 String text5 =  mon5.getText().toString();
+                //火曜日
+                tues = findViewById(R.id.textView41);
+                String Tues = tues.getText().toString();
+                tues1 = findViewById(R.id.text2);
+                String text6 =  tues1.getText().toString();
+                tues2 = findViewById(R.id.text7);
+                String text7 =  tues2.getText().toString();
+                tues3 = findViewById(R.id.text12);
+                String text8 =  tues3.getText().toString();
+                tues4 = findViewById(R.id.text17);
+                String text9 =  tues4.getText().toString();
+                tues5 = findViewById(R.id.text22);
+                String text10 =  tues5.getText().toString();
+                  //水曜日
+                wend = findViewById(R.id.textView42);
+                String Wend = wend.getText().toString();
+                wend1 = findViewById(R.id.text3);
+                String text11 =  wend1.getText().toString();
+                wend2 = findViewById(R.id.text8);
+                String text12 =  wend2.getText().toString();
+                wend3 = findViewById(R.id.text13);
+                String text13 =  wend3.getText().toString();
+                wend4 = findViewById(R.id.text18);
+                String text14 =  wend4.getText().toString();
+                wend5 = findViewById(R.id.text23);
+                String text15 =  wend5.getText().toString();
+                //木曜日
+                thurs = findViewById(R.id.textView43);
+                String Thurs = thurs.getText().toString();
+                thurs1 = findViewById(R.id.text4);
+                String text16 =  thurs1.getText().toString();
+                thurs2 = findViewById(R.id.text9);
+                String text17 =  thurs2.getText().toString();
+                thurs3 = findViewById(R.id.text14);
+                String text18 =  thurs3.getText().toString();
+                thurs4 = findViewById(R.id.text19);
+                String text19 =  thurs4.getText().toString();
+                thurs5 = findViewById(R.id.text24);
+                String text20 =  thurs5.getText().toString();
+                //金曜日
+                fri = findViewById(R.id.textView44);
+                String Fri = fri.getText().toString();
+                fri1 = findViewById(R.id.text5);
+                String text21 =  fri1.getText().toString();
+                fri2 = findViewById(R.id.text10);
+                String text22 =  fri2.getText().toString();
+                fri3 = findViewById(R.id.text15);
+                String text23 =  fri3.getText().toString();
+                fri4 = findViewById(R.id.text20);
+                String text24 =  fri4.getText().toString();
+                fri5 = findViewById(R.id.text25);
+                String text25 =  thurs5.getText().toString();
 
                 // 通知のアイコン
                 builder.setSmallIcon(android.R.drawable.ic_dialog_info);
 
+/*
                 // 通知のタイトル
-
-                builder.setContentTitle("時間割: "+text+"曜日");
-
+                builder.setContentTitle("時間割: "+Mon+"曜日");
                 // 通知の内容
                 builder.setContentText(text1+" | "+text2+" | "+text3+" | "+text4+" | "+text5);
+*/
+
+
+
+
+                //曜日を取得する
+                Calendar calender = Calendar.getInstance();
+
+                String title = null;
+                String memo = null;
+                switch (calender.get(Calendar.DAY_OF_WEEK)) {
+                    case 2:
+                        title = "時間割: " + Mon + "曜日";
+                        memo = text1 + " | " + text2 + " | " + text3 + " | " + text4 + " | " + text5;
+                        break;
+                    case 3:
+                        title = "時間割: " + Tues + "曜日";
+                        memo = text6 + " | " + text7 + " | " + text8 + " | " + text9 + " | " + text10;
+                        break;
+                    case 4:
+                        title = "時間割: "+Wend+"曜日";
+                        memo = text11+" | "+text12+" | "+text13+" | "+text14+" | "+text15;
+                        break;
+                    case 5:
+                        title = "時間割: "+Thurs+"曜日";
+                        memo =  text16+" | "+text17+" | "+text18+" | "+text19+" | "+text20;
+                        break;
+                    case 6:
+                        title = "時間割: "+Fri+"曜日";
+                        memo = text21+" | "+text22+" | "+text23+" | "+text24+" | "+text25;
+                        break;
+
+                }
+                //通知のタイトル
+                builder.setContentTitle(title);
+                // 通知の内容
+                builder.setContentText(memo);
 
                 // 通知をタップした際にアクティビティを起動する
                 // --- ここを削除すると通知の表示のみとなる
